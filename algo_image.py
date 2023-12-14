17aa6ad3ef11bcb2f7832622e8fa88b546d3a9fb
Aziz <aziz.ism98@gmail.com>
2023-12-14 21:42:25 +0100

    Test des fonctions d'erosion, dilatation et ouverture

--- algo_image.py
+++ algo_image.py
@@ -1,17 +1,18 @@
 import cv2
 import numpy as np
 from scipy.ndimage import convolve
-from test_algo import convolve_moi, noyau_gauss, filter_gauss
+from test_algo import convolve_moi, noyau_gauss, filter_gauss, filtre_median, erosion_grayscale, dilatation_grayscale, ouverture_grayscale
 import os
 
 dossier_image = 'Kazakhstan\\files\\domain1\\900900.jpg'
 chemin_image = os.path.join(os.getcwd(), dossier_image)
-print(chemin_image)
+
 image = cv2.imread(chemin_image)
 image_gris = cv2.imread(chemin_image, cv2.IMREAD_GRAYSCALE)
 
-kernel_taille = 15 #nombre impair seulement
-sigma = 2.5
+kernel_taille = 3 #nombre impair seulement
+sigma = 1
+kernel_eros = np.ones((kernel_taille, kernel_taille), np.uint8)
 
 if image is not None:
     print("L'image a été chargée avec succès.")
@@ -24,38 +25,49 @@ np_sobelx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 np_sobely = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
 image_gauss = filter_gauss(image_gris, kernel_taille, sigma)
-print("gausse gauss gauss goss", image_gauss)
+
 image_gauss_uint = image_gauss.astype(np.uint8) #on convertit l'image en uint8 sinon elle ne s'affihe pas
 
 ########################################################################################
 #####                           TEST DES ALGO                                      #####
 
 #test de ma fonction de convolution pour le filtre de sobel
-fct_sobelx = convolve_moi(image_gris,np_sobelx)
-fct_sobely = convolve_moi(image_gris,np_sobely)
+# fct_sobelx = convolve_moi(image_gris,np_sobelx)
+# fct_sobely = convolve_moi(image_gris,np_sobely)
 
-#fonction interne de numpy sobel filter
-cnv_sobelx = convolve(image_gris, np_sobelx)
-cnv_sobely = convolve(image_gris, np_sobely)
+# #fonction interne de numpy sobel filter
+# cnv_sobelx = convolve(image_gris, np_sobelx)
+# cnv_sobely = convolve(image_gris, np_sobely)
 
-####Filtre de Sobel
-sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
-sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+# ####Filtre de Sobel
+# sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+# sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
 
-# Filtre de Canny
-edges = cv2.Canny(image, 100, 200)
+# # Filtre de Canny
+# edges = cv2.Canny(image, 100, 200)
 
-# Transformation de Hough pour la détection des lignes
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
+# # Transformation de Hough pour la détection des lignes
+# lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
+# for line in lines:
+#     x1, y1, x2, y2 = line[0]
+#     cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
-#FILTRE MEDIAN
+# #FILTRE MEDIAN
+# image_median = filtre_median(image_gris, kernel_taille)
+# image_median_uint = image_median.astype(np.uint8)
+
+#erosion
+erosion_img = erosion_grayscale(image_gris, kernel_eros, threshold=90)
+dilatation_img = dilatation_grayscale(image_gris, kernel_eros)
+ouverture_img = ouverture_grayscale(image_gris, kernel_eros)
 
 cv2.imshow('Image Originale', image)
-cv2.imshow('utilisatin gauss', image_gauss_uint)
-cv2.imshow('Sobel X', sobelx)
+# cv2.imshow('utilisatin gauss', image_gauss_uint)
+# cv2.imshow('Sobel X', sobelx)
+# cv2.imshow('filtre median de merde', image_median_uint)
+cv2.imshow('erosion test', erosion_img)
+cv2.imshow('dilat test', dilatation_img)
+cv2.imshow('ouverture', ouverture_img)    
 # cv2.imshow('Sobel Y', sobely)
 # cv2.imshow('Sobel X scipy', cnv_sobelx)
 # cv2.imshow('Sobel Y scipy', cnv_sobely)
--- test_algo.py
+++ test_algo.py
@@ -41,5 +41,54 @@ def filtre_median(image, kernel):
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             sub_matrix = image_pad[i:i+kernel, j:j+kernel]
-            output = np.median(sub_matrix)
+            output[i,j] = np.median(sub_matrix)
     return output
+
+def erosion_grayscale(image, kernel, threshold=127):
+    kernel_height, kernel_width = kernel.shape
+    image_height, image_width = image.shape
+    print("height", kernel_height)
+    output = np.zeros_like(image)
+    print("output image",output)
+
+    pad_height = kernel_height // 2
+    pad_width = kernel_width // 2
+    padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=0)
+
+    for i in range(image_height):
+        for j in range(image_width):
+            sub_matrix = padded_image[i:i+kernel_height, j:j+kernel_width]
+            # Appliquer l'érosion en considérant les valeurs supérieures à threshold comme 1
+            output[i, j] = np.min(sub_matrix[kernel == 1]) > threshold
+    print("iimage finie",output)
+    return output * 255
+
+def dilatation_grayscale(image, kernel):
+    kernel_height, kernel_width = kernel.shape
+    image_height, image_width = image.shape
+
+    # Créer une image de sortie
+    output = np.zeros_like(image)
+
+    # Padding autour de l'image
+    pad_height = kernel_height // 2
+    pad_width = kernel_width // 2
+    padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=0)
+
+    for i in range(image_height):
+        for j in range(image_width):
+            # Extraire la sous-matrice
+            sub_matrix = padded_image[i:i+kernel_height, j:j+kernel_width]
+            # Appliquer la dilatation (maximum des valeurs sous le noyau)
+            output[i, j] = np.max(sub_matrix)
+
+    return output
+
+def ouverture_grayscale(image, kernel):
+    # Appliquer d'abord l'érosion
+    eroded_image = erosion_grayscale(image, kernel)
+
+    # Appliquer ensuite la dilatation sur l'image érodée
+    opened_image = dilatation_grayscale(eroded_image, kernel)
+
+    return opened_image
